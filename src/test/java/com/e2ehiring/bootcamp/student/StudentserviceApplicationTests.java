f96658cb28677a5bff65aaa4eb090fb02ac3cc97
deepak.samria <deepak.samria@diatoz.com>
2023-03-01 11:56:17 +0530

    inital commit - base code

---
+++ src/test/java/com/e2ehiring/bootcamp/student/StudentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2ehiring.bootcamp.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
